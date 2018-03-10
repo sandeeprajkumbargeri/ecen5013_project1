f1be4143f8e57882dbf4de91de0060a989ffa749
Prithvi3141 <Prithvi.Veeravalli@colorado.edu>
2018-03-10 16:57:31 -0700

    b

--- temp_light_sense/source/apds_9301_driver.c
+++ temp_light_sense/source/apds_9301_driver.c
@@ -11,6 +11,29 @@ int apds_9301_init(int i2c_bus_desc)
 	char buf[2];
 	int retval;
 
+        buf[0] = COMMAND_BYTE|TIMING_REGISTER;
+        buf[1] = INTEGRATION_TIME|GAIN_MODE;
+
+        retval = i2c_bus_write(i2c_bus_desc, buf[0], buf+1, 1);
+
+        if(retval < 0)
+        {
+                printf("Error setting the Integration Time and Gain mode %s", strerror(errno));
+                return 1;
+        }
+
+        retval = i2c_bus_read(i2c_bus_desc, buf[0], buf, 1);
+
+        if(retval < 0)
+        {
+                printf("Error reading from I2C slave device; %s", strerror(errno));
+                return 1;
+        }
+
+
+        printf("\n%d\n", (int ) buf[0]);
+
+
 	buf[0] = COMMAND_BYTE|CONTROL_REGISTER;
 	buf[1] = POWER_UP;
 
@@ -39,35 +62,13 @@ int apds_9301_init(int i2c_bus_desc)
 
 	printf("\n%d\n", (int ) buf[0]);
 
-	buf[0] = COMMAND_BYTE|TIMING_REGISTER;
-	buf[1] = INTEGRATION_TIME|GAIN_MODE;
-
-	retval = i2c_bus_write(i2c_bus_desc, buf[0], buf+1, 1);
-
-	if(retval < 0)
-	{
-		printf("Error setting the Integration Time and Gain mode %s", strerror(errno));
-		return 1;
-	}
-
-        retval = i2c_bus_read(i2c_bus_desc, buf[0], buf, 1);
-
-        if(retval < 0)
-        {
-                printf("Error reading from I2C slave device; %s", strerror(errno));
-                return 1;
-        }
-
-
-        printf("\n%d\n", (int ) buf[0]);
-
 	i2c_bus_free();
 	return 0;
 }
 
 int apds_9301_shutdown(int i2c_bus_desc)
 {
-	int buf[2];
+	char buf[2];
 	int retval;
 
 	buf[0] = COMMAND_BYTE|CONTROL_REGISTER;
