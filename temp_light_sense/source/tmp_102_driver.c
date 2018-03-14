e0d53972cb998d1dd5d715710950ffb008afe144
root <root@beaglebone.localdomain>
2018-03-14 00:40:47 +0000

    v1.2 TMP 102 Driver

--- temp_light_sense/source/tmp_102_driver.c
+++ temp_light_sense/source/tmp_102_driver.c
@@ -42,13 +42,14 @@ int tmp_102_read_temperature_reg(int i2c_bus_desc, float *temperature)
 
 	temp_ptr = (uint16_t *)&temp_reg_dig_fmt;
 
-	retval = i2c_bus_write(i2c_bus_desc, TEMPERATURE_REGISTER, buf, 2);
+	retval = i2c_bus_read(i2c_bus_desc, TEMPERATURE_REGISTER, buf, 2);
 	if(retval < 0)
 	{
                 printf("\nError reading the Temp register of the Temp Sensor: TMP 102 %s", strerror(errno));
 		return retval;
 	}
-	buf[1] = buf[1]>>4 + (buf[0]&0x0F)<<4;
+
+	buf[1] = (buf[1]>>4) + ((buf[0]&0x0F)<<4);
 	if(buf[0]&0x80)
 	{
 		buf[0] = ((buf[0]&0x70)>>4)|0xF8;
@@ -97,15 +98,109 @@ int tmp_102_write_config_reg(int i2c_bus_desc, uint16_t config_reg)
 }
 int tmp_102_read_TLow_reg(int i2c_bus_desc, float *TLow_reg)
 {
+        uint8_t buf[2];
+        int retval;
+        int16_t temp_reg_dig_fmt;
+        uint16_t *temp_ptr;
+
+        temp_ptr = (uint16_t *)&temp_reg_dig_fmt;
+
+        retval = i2c_bus_read(i2c_bus_desc, TLOW_REGISTER, buf, 2);
+        if(retval < 0)
+        {
+                printf("\nError reading the TLow register of the Temp Sensor: TMP 102 %s", strerror(errno));
+                return retval;
+        }
+
+        buf[1] = (buf[1]>>4) + ((buf[0]&0x0F)<<4);
+        if(buf[0]&0x80)
+        {
+                buf[0] = ((buf[0]&0x70)>>4)|0xF8;
+        }
+        else
+                buf[0] = (buf[0]&0x70)>>4;
+
+        *temp_ptr = (uint16_t) (buf[1]) + (uint16_t)(buf[0]<<8);
+
+        *TLow_reg = temp_reg_dig_fmt*RESOLUTION;
+
+        return retval;
 
 }
 int tmp_102_read_THigh_reg(int i2c_bus_desc, float *THigh_reg)
 {
+        uint8_t buf[2];
+        int retval;
+        int16_t temp_reg_dig_fmt;
+        uint16_t *temp_ptr;
+
+        temp_ptr = (uint16_t *)&temp_reg_dig_fmt;
+
+        retval = i2c_bus_read(i2c_bus_desc, THIGH_REGISTER, buf, 2);
+        if(retval < 0)
+        {
+                printf("\nError reading the THigh register of the Temp Sensor: TMP 102 %s", strerror(errno));
+                return retval;
+        }
+
+        buf[1] = (buf[1]>>4) + ((buf[0]&0x0F)<<4);
+        if(buf[0]&0x80)
+        {
+                buf[0] = ((buf[0]&0x70)>>4)|0xF8;
+        }
+        else
+                buf[0] = (buf[0]&0x70)>>4;
+
+        *temp_ptr = (uint16_t) (buf[1]) + (uint16_t)(buf[0]<<8);
+
+        *THigh_reg = temp_reg_dig_fmt*RESOLUTION;
+
+        return retval;
 }
 int tmp_102_write_TLow_reg(int i2c_bus_desc, float TLow_register)
 {
+        uint8_t buf[2];
+        int retval;
+        int16_t temp_reg_dig_fmt;
+        uint16_t *temp_ptr;
+
+        temp_ptr = (uint16_t *)&temp_reg_dig_fmt;
+
+	temp_reg_dig_fmt = (int16_t)(TLow_register/RESOLUTION);
+
+	buf[0] = ((uint8_t)((*temp_ptr&0x0F00)>>4) ) + ((uint8_t)((*temp_ptr&0x00F0)>>4));
+	buf[1] = (*temp_ptr&0x000F)<<4;
+
+        retval = i2c_bus_write(i2c_bus_desc, TLOW_REGISTER, buf, 2);
+        if(retval < 0)
+        {
+                printf("\nError writing the TLow register of the Temp Sensor: TMP 102 %s", strerror(errno));
+                return retval;
+        }
+        return retval;
+
 }
-int tmp_102_write_THigh_reg(int i2c_bus_esc, float THigh_reg)
+int tmp_102_write_THigh_reg(int i2c_bus_desc, float THigh_register)
 {
+        uint8_t buf[2];
+        int retval;
+        int16_t temp_reg_dig_fmt;
+        uint16_t *temp_ptr;
+
+        temp_ptr = (uint16_t *)&temp_reg_dig_fmt;
+
+        temp_reg_dig_fmt = (int16_t)(THigh_register/RESOLUTION);
+
+        buf[0] = ((uint8_t)((*temp_ptr&0x0F00)>>4) ) + ((uint8_t)((*temp_ptr&0x00F0)>>4));
+        buf[1] = (*temp_ptr&0x000F)<<4;
+
+        retval = i2c_bus_write(i2c_bus_desc, THIGH_REGISTER, buf, 2);
+        if(retval < 0)
+        {
+                printf("\nError writing the THigh register of the Temp Sensor: TMP 102 %s", strerror(errno));
+                return retval;
+        }
+        return retval;
+
 }
 
